8b48390c90d6efb8b78c60e6726a0e841b8027b0
NETSRV\feitan <gmd29999@yandex.ru>
2024-11-21 22:07:29 +0300

    fix: cors

--- src/modules/test/test.controller.ts
+++ src/modules/test/test.controller.ts
@@ -8,23 +8,7 @@ export class TestController {
   constructor(private readonly testService: TestService) {}
 
   @GrpcMethod('TestService', 'PingPong') // Указываем имя сервиса и метода из .proto
-  pingPong(
-    data: { ping: string },
-    metadata: Metadata,
-    call: ServerUnaryCall<any, any>,
-  ): { pong: string } {
-    const responseMetadata = new Metadata();
-    responseMetadata.set(
-      'Access-Control-Expose-Headers',
-      'grpc-status, grpc-message',
-    );
-    responseMetadata.set('Access-Control-Allow-Origin', '*');
-    responseMetadata.set('Access-Control-Allow-Credentials', 'true');
-    responseMetadata.set('Access-Control-Allow-Headers', '*');
-    responseMetadata.set('Access-Control-Allow-Methods', 'POST, OPTIONS, GET');
-
-    // Отправляем метаданные клиенту
-    call.sendMetadata(responseMetadata);
+  pingPong(data: { ping: string }): { pong: string } {
     return this.testService.pingPong(data);
   }
 }
