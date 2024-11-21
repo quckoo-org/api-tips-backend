a02e43a800123e9921c6db5ec15280923917b274
NETSRV\feitan <gmd29999@yandex.ru>
2024-11-21 19:51:26 +0300

    fix: statuses

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { ReflectionService } from '@grpc/reflection';
-import { join } from 'path';
+import { join, resolve } from 'path';
 
 import { GrpcCorsInterceptor } from './common/interceptors/GrpcCorsInterceptor';
 
@@ -10,16 +10,18 @@ import { GrpcCorsInterceptor } from './common/interceptors/GrpcCorsInterceptor';
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.connectMicroservice<MicroserviceOptions>({
+  const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.GRPC,
     options: {
-      package: ['grpc.health.v1', 'test'], // Specify your proto package name
+      package: ['grpc.health.v1', 'test', 'custom_enums.v1'], // Specify your proto package name
       protoPath: [
         join(__dirname, 'proto/health/v1/health.proto'),
         join(__dirname, 'proto/test/v1/test.proto'),
-      ], // Path to your .proto file
+        join(__dirname, 'proto/custom_enums/v1/custom_enums.proto'),
+      ],
+      loader: {
+        includeDirs: [join(__dirname, 'proto/')], // Указываем директорию с proto
+      },
       url: '0.0.0.0:3000', // Set gRPC server URL,
       onLoadPackageDefinition: (pkg, server) => {
         console.log(
@@ -33,7 +35,7 @@ async function bootstrap() {
   });
 
   app.useGlobalInterceptors(new GrpcCorsInterceptor());
-  app.startAllMicroservices();
+  app.listen();
 }
 
 bootstrap();
--- src/modules/test/test.service.ts
+++ src/modules/test/test.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { PingPongRequest, PingPongResponse } from '../../proto/test/v1/test';
+import { OperationStatus } from '../../proto/custom_enums/v1/custom_enums';
 
 @Injectable()
 export class TestService {
   pingPong(data: PingPongRequest): PingPongResponse {
-    return { pong: `Response to: ${data.ping}` };
+    return {
+      status: OperationStatus.OPERATION_STATUS_OK,
+      pong: `Response to: ${data.ping}`,
+    };
   }
 }
