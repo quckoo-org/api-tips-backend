3443141a99eed9403f1caf2af8b73cf5b8136b7c
NETSRV\faggar <faggar@deeplay.io>
2025-01-10 11:14:34 +0300

    feat: [DP-1089] Имплементация прото-методов (CRUD) для пользователя

--- src/ApiTips.Api/Services/Grpc/Servers/ApiTipsAccessService.cs
+++ src/ApiTips.Api/Services/Grpc/Servers/ApiTipsAccessService.cs
@@ -1,13 +1,17 @@
 using ApiTips.Api.Access.V1;
+using ApiTips.Api.ServiceInterfaces;
 using ApiTips.CustomEnums.V1;
+using ApiTips.Dal;
 using ApiTips.GeneralEntities.V1;
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiTips.Api.Services.Grpc.Servers;
 
-public class ApiTipsAccessService(ILogger<ApiTipsAccessService> logger,IServiceProvider services) : Access.V1.ApiTipsAccessService.ApiTipsAccessServiceBase
+public class ApiTipsAccessService(ILogger<ApiTipsAccessService> logger,IServiceProvider services, IJwtService jwt) : Access.V1.ApiTipsAccessService.ApiTipsAccessServiceBase
 {
-    public async override Task<GetUsersResponse> GetUsers(GetUsersRequest request, ServerCallContext context)
+    public override async Task<GetUsersResponse> GetUsers(GetUsersRequest request, ServerCallContext context)
     {
         // Дефолтный объект
         var response = new GetUsersResponse
@@ -18,12 +22,60 @@ public class ApiTipsAccessService(ILogger<ApiTipsAccessService> logger,IServiceP
             }
         };
         
-        // code
+        // Получение контекста базы данных из сервисов коллекций
+        await using var scope = services.CreateAsyncScope();
+        await using var applicationContext = scope.ServiceProvider.GetRequiredService<ApplicationContext>();
+
+
+        var users = applicationContext.Users.AsNoTracking();
+        
+        // Фильтрация по Email пользователя
+        if (!string.IsNullOrWhiteSpace(request.Filter.Email))
+            users = users.Where(user => user.Email == request.Filter.Email);
+        
+        // Получение только заблокированных пользователей
+        if (request.Filter.IsBlocked == true)
+            users = users.Where(user => user.LockDateTime < DateTime.UtcNow);
+        
+        // Получение только верифицированных пользователей
+        if (request.Filter.IsVerified == true)
+            users = users.Where(user => user.VerifyDateTime != null);
+        
+        // Получение только удаленных пользователей
+        if (request.Filter.IsDeleted == true)
+            users = users.Where(user => user.DeleteDateTime != null);
+        
+        // Отправка запроса после фильтрации в Базу данных
+        var result = await users.ToListAsync(context.CancellationToken);
+        
+        if (result.Count == 0)
+        {
+            response.Response.Status = OperationStatus.NoData;
+            response.Response.Description = "Не найдены пользователи по заданным фильтрам";
+            return response;
+        }
+        
+        // Маппинг пользователей из БД в ответ
+        // TODO перенести в AutoMapper
+        response.Users.AddRange(result.Select(user => new User
+        {
+            Id = user.Id,
+            Email = user.Email,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            Cca3= user.Cca3,
+            CreatedAt = user.CreateDateTime.ToTimestamp(),
+            BlockedAt = user.LockDateTime?.ToTimestamp(),
+            DeletedAt = user.DeleteDateTime?.ToTimestamp(),
+            VerifiedAt = user.VerifyDateTime?.ToTimestamp()
+        }));
+
+        response.Response.Status = OperationStatus.Ok;
         
         return response;
     }
     
-    public async override Task<GetUserResponse> GetUser(GetUserRequest request, ServerCallContext context)
+    public override async Task<GetUserResponse> GetUser(GetUserRequest request, ServerCallContext context)
     {
         // Дефолтный объект
         var response = new GetUserResponse
@@ -34,7 +86,37 @@ public class ApiTipsAccessService(ILogger<ApiTipsAccessService> logger,IServiceP
             }
         };
         
-        // code
+        // Получение контекста базы данных из сервисов коллекций
+        await using var scope = services.CreateAsyncScope();
+        await using var applicationContext = scope.ServiceProvider.GetRequiredService<ApplicationContext>();
+        
+        var user = await applicationContext.Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(user => user.Id == request.UserId,
+                context.CancellationToken);
+
+        if (user is null)
+        {
+            response.Response.Status = OperationStatus.NoData;
+            response.Response.Description = "Не найден пользователь по заданному ID";
+            
+            return response;
+        }
+
+        // Маппинг пользователя из БД в ответ
+        // TODO перенести в AutoMapper
+        response.User = new User
+        {
+            Id = user.Id,
+            Email = user.Email,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            Cca3= user.Cca3,
+            CreatedAt = user.CreateDateTime.ToTimestamp(),
+            BlockedAt = user.LockDateTime?.ToTimestamp(),
+            DeletedAt = user.DeleteDateTime?.ToTimestamp(),
+            VerifiedAt = user.VerifyDateTime?.ToTimestamp()
+        };
         
         return response;
     }
@@ -50,12 +132,69 @@ public class ApiTipsAccessService(ILogger<ApiTipsAccessService> logger,IServiceP
             }
         };
         
-        // code
+        // Получение контекста базы данных из сервисов коллекций
+        await using var scope = services.CreateAsyncScope();
+        await using var applicationContext = scope.ServiceProvider.GetRequiredService<ApplicationContext>();
+
+        // Проверка на дублирование по почте пользователя
+        var existingUser = await applicationContext.Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Email == request.Email);
+        if (existingUser is not null)
+        {
+            response.Response.Status = OperationStatus.Duplicate;
+            response.Response.Description = "Пользователь с таким Email уже существует";
+            
+            return response;
+        }
         
+        var roles = await applicationContext.Roles
+            .Where(role => request.RolesIds.Contains(role.Id))
+            .ToListAsync(context.CancellationToken);
+        if (roles.Count == 0)
+        {
+            response.Response.Status = OperationStatus.Error;
+            response.Response.Description = "Не найдены роли по заданным идентификаторам";
+            
+            return response;
+        }
+        
+        var userToAdd = new ApiTips.Dal.schemas.system.User
+        {
+            Email = request.Email,
+            Password = request.Password,
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Cca3 = request.Cca3,
+            Roles = roles
+        };
+        
+        await applicationContext.Users.AddAsync(userToAdd, context.CancellationToken);
+        try
+        {
+            if (await applicationContext.SaveChangesAsync(context.CancellationToken) == 0)
+            {
+                response.Response.Status = OperationStatus.Error;
+                response.Response.Description = "Ошибка добавления пользователя в БД";
+                
+                return response;
+            }
+        }
+        catch (Exception e)
+        {
+            logger.LogError("Ошибка добавления пользователя в БД: {Message} | InnerException: {InnerMessage}",
+                e.Message, e.InnerException?.Message);
+            
+            response.Response.Status = OperationStatus.Error;
+            response.Response.Description = "Ошибка добавления пользователя в БД";
+        }
+
+        response.Response.Status = OperationStatus.Ok;
+            
         return response;
     }
 
-    public async override Task<UpdateUserResponse> UpdateUser(UpdateUserRequest request, ServerCallContext context)
+    public override async Task<UpdateUserResponse> UpdateUser(UpdateUserRequest request, ServerCallContext context)
     {
         // Дефолтный объект
         var response = new UpdateUserResponse
@@ -66,11 +205,73 @@ public class ApiTipsAccessService(ILogger<ApiTipsAccessService> logger,IServiceP
             }
         };
         
-        // code
+        // Получение контекста базы данных из сервисов коллекций
+        await using var scope = services.CreateAsyncScope();
+        await using var applicationContext = scope.ServiceProvider.GetRequiredService<ApplicationContext>();
+        
+        var user = await applicationContext.Users
+            .Include(user => user.Roles)
+            .FirstOrDefaultAsync(user => user.Id == request.UserId,
+                context.CancellationToken);
+
+        if (user is null)
+        {
+            response.Response.Description = "Не найден пользователь по заданному идентификатору";
+            response.Response.Status = OperationStatus.NoData;
+            return response;
+        }
+
+        if (request.HasEmail)
+            user.Email = request.Email;
+        if (request.HasFirstName)
+            user.FirstName = request.FirstName;
+        if (request.HasLastName)
+            user.LastName = request.LastName;
+        if (request.HasCca3)
+            user.Cca3 = request.Cca3;
+        if (request.HasIsVerified)
+            user.VerifyDateTime = request.IsVerified ? DateTime.UtcNow : null;
+        if (request.HasIsBlocked)
+            user.LockDateTime = request.IsBlocked ? DateTime.UtcNow : null;
+        if (request.HasIsDeleted)
+            user.DeleteDateTime = request.IsDeleted ? DateTime.UtcNow : null;
+        if (request.RolesIds.Count > 0)
+           user.Roles = await GetRolesAsync(applicationContext, request.RolesIds.ToList(), context.CancellationToken);
+
+
+        try
+        {
+            if (await applicationContext.SaveChangesAsync(context.CancellationToken) == 0)
+            {
+                response.Response.Status = OperationStatus.Error;
+                response.Response.Description = "Ошибка обновления пользователя в БД";
+                logger.LogError("Ошибка обновления пользователя в БД");
+                
+                return response;
+            }
+        }
+        catch (Exception e)
+        {
+            response.Response.Status = OperationStatus.Error;
+            response.Response.Description = "Ошибка обновления пользователя в БД";
+            logger.LogError("Ошибка во время обновления пользователя в БД");
+                
+            return response;
+        }
+
+        response.Response.Status = OperationStatus.Ok;
         
         return response;
     }
-
+    
+    private async Task<List<ApiTips.Dal.schemas.system.Role>> GetRolesAsync(ApplicationContext context, 
+        List<long> ids, CancellationToken token)
+    {
+        return await context.Roles
+            .Where(role => ids.Contains(role.Id))
+            .ToListAsync(token);
+    }
+    
     public override async Task<GetRolesResponse> GetRoles(GetRolesRequest request, ServerCallContext context)
     {
         // Дефолтный объект
@@ -82,7 +283,35 @@ public class ApiTipsAccessService(ILogger<ApiTipsAccessService> logger,IServiceP
             }
         };
         
-        // code
+        // Получение контекста базы данных из сервисов коллекций
+        await using var scope = services.CreateAsyncScope();
+        await using var applicationContext = scope.ServiceProvider.GetRequiredService<ApplicationContext>();
+        
+        var roles = await applicationContext
+            .Roles
+            .AsNoTracking()
+            .Include(x => x.Permissions)
+            .ThenInclude(x => x.Methods)
+            .ToListAsync();
+
+        if (roles.Count == 0)
+        {
+            logger.LogWarning("Не найдены роли в базе данных");
+            response.Response.Status = OperationStatus.NoData;
+            response.Response.Description = "Не найдены роли в базе данных";
+
+            return response;
+        }
+        
+        // Маппинг ролей из БД в ответ
+        // TODO перенести в AutoMapper
+        response.Roles.AddRange(roles.Select(role => new Role
+        {
+            Id = role.Id,
+            Name = role.Name,
+        }));
+
+        response.Response.Status = OperationStatus.Ok;
         
         return response;
     }
@@ -99,6 +328,37 @@ public class ApiTipsAccessService(ILogger<ApiTipsAccessService> logger,IServiceP
         };
         
         // code
+        // Получение контекста базы данных из сервисов коллекций
+        await using var scope = services.CreateAsyncScope();
+        await using var applicationContext = scope.ServiceProvider.GetRequiredService<ApplicationContext>();
+
+        if (request.RoleId is null)
+        {
+            response.Response.Status = OperationStatus.Declined;
+            response.Response.Description = "Не указан идентификатор роли";
+
+            return response;
+        }
+        
+        var role = await applicationContext.Roles
+            .FirstOrDefaultAsync(x => x.Id == request.RoleId, context.CancellationToken);
+
+        if (role is null)
+        {
+            response.Response.Status = OperationStatus.NoData;
+            response.Response.Description = "Не найдена роль по заданному идентификатору";
+
+            return response;
+        }
+        
+        // Маппинг роли из БД в ответ
+        // TODO перенести в AutoMapper
+        response.Role = new Role
+        {
+            Id = role.Id,
+            Name = role.Name,
+        };
+        response.Response.Status = OperationStatus.Ok;
         
         return response;
     }
