b2ed14b086a084687b30f1a9811c0eba54ac6407
DESKTOP-N9KKR1T\Tadius <vadim.reskov@mail.ru>
2025-01-15 16:56:10 +0600

    fix: [DP-443/DP-1152] Исправлие маппера и прото тарифа

--- src/ApiTips.Api/MapperProfiles/Tariff/TariffProfile.cs
+++ src/ApiTips.Api/MapperProfiles/Tariff/TariffProfile.cs
@@ -12,10 +12,11 @@ public class TariffProfile : Profile
                 opt.MapFrom(src => src.Id))
             .ForMember(dst => dst.Name, opt =>
                 opt.MapFrom(src => src.Name))
-            // .ForMember(dst => dst.TipPrice, opt =>
-            //     opt.MapFrom(src => src.TipPrice.ToDecimal()))
-            .ForMember(dst => dst.TipPrice, opt =>
-                opt.Ignore())
+             .ForMember(dst => dst.TipPrice, opt =>
+             {
+                 opt.PreCondition(src => src.TipPrice != null);
+                 opt.MapFrom(src => src.TipPrice!.Value.ToDecimal());
+             })
              .ForMember(dst => dst.FreeTipsCount, opt =>
                 opt.MapFrom(src => src.FreeTipsCount))
              .ForMember(dst => dst.PaidTipsCount, opt =>
