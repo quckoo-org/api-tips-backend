a62a3a8fb18736d8aaa7572a627053a42e69ae4a
DESKTOP-N9KKR1T\Tadius <vadim.reskov@mail.ru>
2025-01-20 14:42:14 +0600

    feat: [DP-1208] Добавлена реализация прото для работы с заказами на заглушках

---
+++ src/ApiTips.Api/MapperProfiles/Order/OrderProfile.cs
@@ -0,0 +1,15 @@
+﻿using ApiTips.Api.Extensions.Grpc;
+using AutoMapper;
+
+namespace ApiTips.Api.MapperProfiles.Order;
+
+public class OrderProfile : Profile
+{
+    public OrderProfile()
+    {
+        //CreateMap<Dal.schemas.data.Order, Api.Order.V1.Order>()
+        //    .ForMember(dst => dst.Id, opt =>
+        //        opt.MapFrom(src => src.Id))
+        //    ;
+    }
+}
---
+++ src/ApiTips.Api/Services/Grpc/Servers/ApiTipsOrderService.cs
@@ -0,0 +1,122 @@
+﻿using ApiTips.Api.Access.V1;
+using ApiTips.Api.Extensions.Grpc;
+using ApiTips.Api.MapperProfiles.Order;
+using ApiTips.Api.Order.V1;
+using ApiTips.CustomEnums.V1;
+using ApiTips.Dal;
+using ApiTips.GeneralEntities.V1;
+using AutoMapper;
+using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiTips.Api.Services.Grpc.Servers;
+
+public class ApiTipsOrderService:
+    Order.V1.ApiTipsOrderService.ApiTipsOrderServiceBase
+{
+    /// <summary>
+    ///     Логгер
+    /// </summary>
+    private readonly ILogger<ApiTipsOrderService> _logger;
+
+    /// <summary>
+    ///     Маппер
+    /// </summary>
+    private readonly IMapper _mapper;
+
+    public ApiTipsOrderService(IHostEnvironment env, ILogger<ApiTipsOrderService> logger, IServiceProvider services)
+    {
+        _logger = logger;
+        Services = services;
+
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AllowNullCollections = true;
+            cfg.AllowNullDestinationValues = true;
+            cfg.AddProfile(typeof(OrderProfile));
+        });
+
+        if (env.IsDevelopment())
+        {
+            config.CompileMappings();
+            config.AssertConfigurationIsValid();
+        }
+
+        _mapper = new Mapper(config);
+    }
+
+    /// <summary>
+    ///     Зарегистрированные сервисы
+    /// </summary>
+    private IServiceProvider Services { get; }
+
+    public override async Task<GetOrdersResponse> GetOrders(GetOrdersRequest request, ServerCallContext context)
+    {
+        // Дефолтный объект
+        var response = new GetOrdersResponse
+        {
+            Response = new GeneralResponse
+            {
+                Status = OperationStatus.Unspecified
+            }
+        };
+
+        return response;
+    }
+
+    public override async Task<GetOrderResponse> GetOrder(GetOrderRequest request, ServerCallContext context)
+    {
+        // Дефолтный объект
+        var response = new GetOrderResponse
+        {
+            Response = new GeneralResponse
+            {
+                Status = OperationStatus.Unspecified
+            }
+        };
+
+        return response;
+    }
+
+    public override async Task<AddOrderResponse> AddOrder(AddOrderRequest request, ServerCallContext context)
+    {
+        // Дефолтный объект
+        var response = new AddOrderResponse
+        {
+            Response = new GeneralResponse
+            {
+                Status = OperationStatus.Unspecified
+            }
+        };
+
+        return response;
+    }
+
+    public override async Task<SetOrderStatusPaidResponse> SetOrderStatusPaid(SetOrderStatusPaidRequest request, ServerCallContext context)
+    {
+        // Дефолтный объект
+        var response = new SetOrderStatusPaidResponse
+        {
+            Response = new GeneralResponse
+            {
+                Status = OperationStatus.Unspecified
+            }
+        };
+
+        return response;
+    }
+
+    public override async Task<SetOrderStatusCancelledResponse> SetOrderStatusCancelled(SetOrderStatusCancelledRequest request, ServerCallContext context)
+    {
+        // Дефолтный объект
+        var response = new SetOrderStatusCancelledResponse
+        {
+            Response = new GeneralResponse
+            {
+                Status = OperationStatus.Unspecified
+            }
+        };
+
+        return response;
+    }
+}
