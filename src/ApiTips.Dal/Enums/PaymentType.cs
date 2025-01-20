8613f298cac309b67cd2fe1d9b70ccc3909ed097
NETSRV\faggar <faggar@deeplay.io>
2025-01-20 15:20:12 +0300

    feat:[DP-1214] БД Банковского счёта

---
+++ src/ApiTips.Dal/Enums/PaymentType.cs
@@ -0,0 +1,8 @@
+namespace ApiTips.Dal.Enums;
+
+public enum PaymentType
+{
+    Unspecified = 0,
+    Crypto = 1,
+    Money = 2,
+}
---
+++ src/ApiTips.Dal/schemas/data/BankAccount.cs
@@ -0,0 +1,64 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using ApiTips.Dal.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiTips.Dal.schemas.data;
+
+public class BankAccount
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [ConcurrencyCheck]
+    [Comment("Уникальный идентификатор банковского счёта")]
+    public long Id { get; set; }
+    
+    [Required]
+    [MinLength(1)]
+    [MaxLength(1000)]
+    [ConcurrencyCheck]
+    [Comment("Наименование банка")]
+    public required string BankName { get; set;}
+    
+    [Required]
+    [MinLength(1)]
+    [MaxLength(1000)]
+    [ConcurrencyCheck]
+    [Comment("Адрес банка")]
+    public required string BankAddress { get; set;}
+    
+    [Required]
+    [MinLength(8)]
+    [MaxLength(11)]
+    [ConcurrencyCheck]
+    [Comment("Swift номер")]
+    public required string Swift { get; set;}
+    
+    [Required]
+    [MinLength(8)]
+    [MaxLength(11)]
+    [ConcurrencyCheck]
+    [Comment("Номер аккаунта")]
+    public required string AccountNumber { get; set;}
+    
+    [Required]
+    [Length(34, 34)]
+    [ConcurrencyCheck]
+    [Comment("Iban номер")]
+    public required string Iban { get; set;}
+    
+    [MinLength(1)]
+    [MaxLength(255)]
+    [ConcurrencyCheck]
+    [Comment("Дополнительная информация")]
+    public string? AdditionalInfo { get; set;}
+
+
+    [ConcurrencyCheck]
+    [Comment("Признак запрета счёта")]
+    public bool IsBaned { get; set; } = false;
+    
+    [ConcurrencyCheck]
+    [Comment("Способ оплаты")]
+    public PaymentType PaymentType { get; set; }
+}
