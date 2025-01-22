333349adfc8706d0020c8706cbd1a4a6fcbbe46b
DESKTOP-N9KKR1T\Tadius <vadim.reskov@mail.ru>
2025-01-22 16:40:02 +0600

    feat: [DP-1220] Добавлена модель баланса с историей измнений в DAL

---
+++ src/ApiTips.Dal/schemas/data/Balance.cs
@@ -0,0 +1,39 @@
+﻿using ApiTips.Dal.schemas.system;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ApiTips.Dal.schemas.data;
+
+[Table(nameof(Balance), Schema = "data")]
+[Comment("Объект - баланс")]
+public class Balance
+{
+    [Key, ForeignKey("User")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [ConcurrencyCheck]
+    [Comment("Уникальный идентификатор баланса")]
+    public long Id { get; set; }
+
+    [ConcurrencyCheck]
+    [Comment("Количество бесплатных подсказок")]
+    public long FreeTipsCount { get; set; }
+
+    [ConcurrencyCheck]
+    [Comment("Количество оплаченных подсказок")]
+    public long PaidTipsCount { get; set; }
+
+    [NotMapped]
+    [Comment("Общее количество подсказок, вычисляемое поле")]
+    public long TotalTipsCount => FreeTipsCount + PaidTipsCount;
+
+    [ConcurrencyCheck]
+    [Required]
+    [Comment("Пользователь - владелец баланса")]
+    public required User User { get; set; }
+
+    [ConcurrencyCheck]
+    [Comment("Баланс, на изменение которого была направлена операция")]
+    public List<BalanceHistory> History { get; set; } = [];
+}
